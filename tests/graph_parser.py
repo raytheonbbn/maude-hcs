a66481823c1590a19bee9aaecd3f2c85098a2d8b
Christophe Merlin <Christophe.Merlin@raytheon.com>
2025-05-20 16:40:32 -0400

    Tests: Update latency and jitter as strings
    Latency and jitter are passed as a string with s and ms (and maybe us)
    in the shadow files.
    
    Make sure to pull the latest changes on DNS-formalization repo.

--- tests/graph_parser.py
+++ tests/graph_parser.py
@@ -16,10 +16,10 @@ graph [
   node [ id 0 label "HostA" asn 100 ip_addr "10.0.0.1" host_bandwidth_up 100000000 host_bandwidth_down 100000000 ]
   node [ id 1 label "Router1" asn 200 ]
   node [ id 2 label "HostB" asn 100 ip_addr "10.0.0.2" host_bandwidth_up 50000000 host_bandwidth_down 100000000 ]
-  edge [ source 0 target 1 label "Link_HostA_to_Router1" latency 10.5 jitter 1.2 packet_loss 0.01 ]
-  edge [ source 1 target 0 label "Link_Router1_to_HostA" latency 10.5 jitter 0.0 packet_loss 0.0 ]
-  edge [ source 1 target 2 label "Link_Router1_to_HostB" latency 15.0 jitter 2.0 packet_loss 0.005 ]
-  edge [ source 2 target 1 label "Link_HostB_to_Router1" latency 15.0 jitter 1.8 packet_loss 0.008 ]
+  edge [ source 0 target 1 label "Link_HostA_to_Router1" latency "10.5ms" jitter "1.2ms" packet_loss 0.01 ]
+  edge [ source 1 target 0 label "Link_Router1_to_HostA" latency "10.5ms" jitter "0.0ms" packet_loss 0.0 ]
+  edge [ source 1 target 2 label "Link_Router1_to_HostB" latency "15.0ms" jitter "2.0ms" packet_loss 0.005 ]
+  edge [ source 2 target 1 label "Link_HostB_to_Router1" latency "15.0ms" jitter "1.8ms" packet_loss 0.008 ]
 ]
 """
 
