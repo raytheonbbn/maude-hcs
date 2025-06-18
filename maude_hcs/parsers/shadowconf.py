89a06608b610df42d78d86b72ec33f6e42e02311
Christophe Merlin <Christophe.Merlin@raytheon.com>
2025-06-18 15:40:58 -0400

    Parser: Add markings
    No functional change.

--- maude_hcs/parsers/shadowconf.py
+++ maude_hcs/parsers/shadowconf.py
@@ -1,3 +1,32 @@
+#!/usr/bin/env python
+# MAUDE_HCS: maude_hcs
+#
+# Software Markings (UNCLASS)
+# PWNDD Software
+#
+# Copyright (C) 2025 RTX BBN Technologies Inc. All Rights Reserved
+#
+# Contract No: HR00112590083
+# Contractor Name: RTX BBN Technologies Inc.
+# Contractor Address: 10 Moulton Street, Cambridge, Massachusetts 02138
+#
+# The U.S. Government's rights to use, modify, reproduce, release, perform,
+# display, or disclose these technical data and software are defined in the
+# Article VII: Data Rights clause of the OTA.
+#
+# This document does not contain technology or technical data controlled under
+# either the U.S. International Traffic in Arms Regulations or the U.S. Export
+# Administration Regulations.
+#
+# DISTRIBUTION STATEMENT A: Approved for public release; distribution is
+# unlimited.
+#
+# Notice: Markings. Any reproduction of this computer software, computer
+# software documentation, or portions thereof must also reproduce the markings
+# contained herein.
+#
+# MAUDE_HCS: end
+
 import logging
 from pathlib import Path
 import yaml
