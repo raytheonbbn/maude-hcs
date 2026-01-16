4794fac6784d041956e267e64db11a9f67354f47
Joud Khoury <joud.khoury@rtx.com>
2026-01-16 18:17:03 -0500

    script to auto gen models and scenarios

---
+++ scripts/generate_cp2_maude.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Loop from 1 to 12 for each scenario
+for i in {1..12}
+do
+    echo "Running commands for scenario $i..."
+    # Command 1: raceboat Markov generation
+    maude-hcs --verbose --protocol=mastodon markov \
+        --json-dir="../pwnd-cp2/cp2_scenarios/cp2_scenario_$i/actions/" \
+        --maude-dir="./maude_hcs/lib/raceboat/maude/mastodonprofiles/"
+
+    # Command 2: Generate maude init config
+    maude-hcs --verbose generate \
+        --yml-filename="../pwnd-cp2/cp2_scenarios/cp2_scenario_$i/cp2_scenario_$i.yml" \
+        --model=prob \
+        --filename="cp2_scenario_$i"
+
+    echo "Finished scenario $i."
+    echo "-----------------------------------"
+done
