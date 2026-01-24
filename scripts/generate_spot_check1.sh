e34752e4c8da08e9ccb9bdd5709a05ab9e815e23
Joud Khoury <joud.khoury@rtx.com>
2026-01-24 14:18:02 -0500

    script to generate spot check

---
+++ scripts/generate_spot_check1.sh
@@ -0,0 +1,2 @@
+ maude-hcs --verbose --protocol=mastodon markov  --json-dir=./use-cases/challenge-problem-2/spot-check-1/actions/ --maude-dir=./maude_hcs/lib/raceboat/maude/mastodonprofiles/
+ maude-hcs --verbose generate --yml-filename=./use-cases/challenge-problem-2/spot-check-1/spot_check_1.yml --model=prob --filename=spot_check_1 --output-dir=./use-cases/challenge-problem-2/spot-check-1/maude/
