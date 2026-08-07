248dcb616804a0c38f260333f13fda3704f8d72a
luke-west-bbn <Luke.S.West@rtx.com>
2026-08-07 14:26:04 -0400

    updated formatting script to handle dumps for individual runs, and committed the little bash script I've been using for tests

---
+++ runtest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+rootdir=/Users/lwest/Documents/pwnd2/playground-maude-hcs
+
+maude-hcs scheck \
+    --file $rootdir/maude_hcs/lib/smc/smc_cp3.maude \
+    --test $rootdir/use-cases/challenge-problem-3/cp3_scenarios/scenario1/scenario1.maude \
+    --query $rootdir/smc/cp3_eval.quatex \
+    --dump $rootdir/dump.txt \
+    -j 1 -n 10-12
+
+    # --test $rootdir/use-cases/challenge-problem-3/cp3_scenarios/scenario1/only_webtunnel.maude \
--- scripts/format_for_tne.py
+++ scripts/format_for_tne.py
@@ -1,5 +1,6 @@
 import sys
 import json
+from pathlib import Path
 from datetime import datetime
 
 # For all of these, we need separate values for separate windows!
@@ -40,6 +41,10 @@ adv_keys = [
     "tcp_new_conn_count",
 ]
 
+len_perf_latencies = len(perf_latency_keys)
+len_perf_other = len(perf_other_keys)
+len_adv = len(adv_keys)
+
 def mk_windows(lst: list, sizes: list[int]) -> list[list]:
     assert len(lst) == sum(sizes)
     idx = 0
@@ -53,49 +58,77 @@ def mean_stddev_rad(line: str) -> tuple[float, float, float]:
     toks = line.split()
     return (float(toks[2]), float(toks[5]), float(toks[8]))
 
+def list_to_perf_dict(lst: list[float]) -> dict:
+    assert len(lst) == len_perf_latencies + len_perf_other
+    result = {"latency": {}}
+    perf_latency_stats, perf_other_stats = tuple(
+        mk_windows(lst, [len_perf_latencies, len_perf_other])
+    )
+
+    for stat, key in zip(perf_latency_stats, perf_latency_keys):
+        result["latency"][key] = stat
+
+    for stat, key in zip(perf_other_stats, perf_other_keys):
+        result[key] = stat
+
+    return result
+
+def list_to_adv_dict(lst: list[float], scenario: str, generated_at: str) -> dict:
+    assert len(lst) == 2 * len_adv
+    result = {}
+    result["metadata"] = {"scenario": scenario, "generated_at": generated_at}
+    adv_cum_stats, adv_ind_stats = tuple(
+        mk_windows(lst, [len_adv, len_adv])
+    )
+
+    for stat, key in zip(adv_cum_stats, adv_keys):
+        result[key] = {"cumulative": stat}
+
+    for stat, key in zip(adv_ind_stats, adv_keys):
+        result[key] = {"independent": stat}
+
+    return result
+
 if __name__ == "__main__":
     input_file = sys.argv[1]
     perf_output_file = sys.argv[2]
     adv_output_file = sys.argv[3]
+    dump_file = sys.argv[4]
+    run_output_dir = sys.argv[5]
+
     scenario = input_file.removesuffix(".txt")
     generated_at = str(datetime.now().isoformat())
-    perf_result = {}
-    adv_result = {"metadata": {"scenario": scenario, "generated_at": generated_at}}
 
     with open(input_file, "r") as f:
         lines = [ln.strip() for ln in f.readlines() if ln.strip().startswith("μ")]
-        for line in lines:
-            print(line)
 
-        len_perf_latencies = len(perf_latency_keys)
-        len_perf_other = len(perf_other_keys)
-        len_adv = len(adv_keys)
+    means = [mean_stddev_rad(line)[0] for line in lines]
 
-        # len_adv is repeated because there are cumulative and independent versions
-        assert len_perf_latencies + len_perf_other + (2*len_adv) == len(lines)
+    # len_adv is repeated because there are cumulative and independent versions
+    assert len_perf_latencies + len_perf_other + (2*len_adv) == len(means)
 
-        perf_latency_stats, perf_other_stats, adv_cum_stats, adv_ind_stats = tuple(
-            mk_windows(lines, [len_perf_latencies, len_perf_other, len_adv, len_adv])
-        )
-        perf_result["latency"] = {}
-        for line, key in zip(perf_latency_stats, perf_latency_keys):
-            stats = mean_stddev_rad(line)
-            perf_result["latency"][key] = stats[0]
 
-        for line, key in zip(perf_other_stats, perf_other_keys):
-            stats = mean_stddev_rad(line)
-            perf_result[key] = stats[0]
+    perf_dict = list_to_perf_dict(means[:len_perf_latencies + len_perf_other])
+    adv_dict = list_to_adv_dict(means[len_perf_latencies + len_perf_other:], scenario, generated_at)
 
-        for line, key in zip(adv_cum_stats, adv_keys):
-            stats = mean_stddev_rad(line)
-            adv_result[key] = {"cumulative": stats[0]}
+    with open(perf_output_file, "w") as f:
+        json.dump(perf_dict, f, indent=4)
 
-        for line, key in zip(adv_ind_stats, adv_keys):
-            stats = mean_stddev_rad(line)
-            adv_result[key] = {"independent": stats[0]}
+    with open(adv_output_file, "w") as f:
+        json.dump(adv_dict, f, indent=4)
 
-        with open(perf_output_file, "w") as g:
-            json.dump(perf_result, g, indent=4)
+    with open(dump_file, "r") as f:
+        runs = [[float(samp) for samp in line.split()] for line in f.readlines()]
 
-        with open(adv_output_file, "w") as h:
-            json.dump(adv_result, h, indent=4)
+    for i, run in enumerate(runs):
+        run_perf_dict = list_to_perf_dict(run[:len_perf_latencies + len_perf_other])
+        run_adv_dict = list_to_adv_dict(run[len_perf_latencies + len_perf_other:], scenario, generated_at)
+
+        run_perf_output_file = Path(run_output_dir) / f"run{i}_perf.json"
+        run_adv_output_file = Path(run_output_dir) / f"run{i}_adv.json"
+
+        with open(run_perf_output_file, "w") as f:
+            json.dump(run_perf_dict, f, indent=4)
+
+        with open(run_adv_output_file, "w") as f:
+            json.dump(run_adv_dict, f, indent=4)
