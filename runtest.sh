a7b26454952b886d878e08a4e12acaec67806f53
luke-west-bbn <Luke.S.West@rtx.com>
2026-08-10 16:49:10 -0400

    initial commit for this branch to generate quatex

--- runtest.sh
+++ runtest.sh
@@ -6,8 +6,9 @@ rootdir=/Users/lwest/Documents/pwnd2/playground-maude-hcs
 maude-hcs scheck \
     --file $rootdir/maude_hcs/lib/smc/smc_cp3.maude \
     --test $rootdir/use-cases/challenge-problem-3/cp3_scenarios/scenario1/scenario1.maude \
-    --query $rootdir/smc/cp3_eval.quatex \
+    --query $rootdir/test.quatex \
     --dump $rootdir/dump.txt \
-    -j 1 -n 10-12
+    --format json \
+    -j 1 -n 1-1 \
 
     # --test $rootdir/use-cases/challenge-problem-3/cp3_scenarios/scenario1/only_webtunnel.maude \
