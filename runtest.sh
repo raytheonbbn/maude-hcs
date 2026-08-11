1049aade386f331ae284ac99b7e3003eb3930b29
luke-west-bbn <Luke.S.West@rtx.com>
2026-08-11 15:51:10 -0400

    made features more configurable in generate_quatex.py and updated README

--- runtest.sh
+++ runtest.sh
@@ -4,19 +4,19 @@ set -euo pipefail
 rootdir=/Users/lwest/Documents/pwnd2/playground-maude-hcs
 
 python3 ./scripts/cp3_glue/generate_quatex.py test.quatex
-# dumpdir=$(python3 ./scripts/cp3_glue/mk_dump_dir.py dumps/dump)
-dumpdir=./dumps/dump2026-08-11_13-56-04
+dumpdir=$(python3 ./scripts/cp3_glue/mk_dump_dir.py dumps/dump)
+# dumpdir=./dumps/dump2026-08-11_13-56-04
 
-# maude-hcs scheck \
-#     --file $rootdir/maude_hcs/lib/smc/smc_cp3.maude \
-#     --test $rootdir/use-cases/challenge-problem-3/cp3_scenarios/scenario1/scenario1.maude \
-#     --query $rootdir/test.quatex \
-#     --dump $dumpdir/dump.txt \
-#     -j 1 -n 1 \
-#     | tee log.txt
-#
-# cat $dumpdir/dump.txt.* > $dumpdir/combined_dump.txt
-# rm $dumpdir/dump.txt.*
+maude-hcs scheck \
+    --file $rootdir/maude_hcs/lib/smc/smc_cp3.maude \
+    --test $rootdir/use-cases/challenge-problem-3/cp3_scenarios/scenario1/scenario1.maude \
+    --query $rootdir/test.quatex \
+    --dump $dumpdir/dump.txt \
+    -j 1 -n 1 \
+    | tee log.txt
+
+cat $dumpdir/dump.txt.* > $dumpdir/combined_dump.txt
+rm $dumpdir/dump.txt.*
 
 python3 ./scripts/cp3_glue/format_for_tne_v2.py \
     --smc-results-file log.txt \
