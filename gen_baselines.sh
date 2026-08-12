c87412e19237332712c4983554ceba814a373366
luke-west-bbn <Luke.S.West@rtx.com>
2026-08-12 18:13:32 -0400

    removed unnecessary line from gen_baselines.sh

--- gen_baselines.sh
+++ gen_baselines.sh
@@ -6,12 +6,11 @@ rootdir=/Users/lwest/Documents/pwnd2/playground-maude-hcs
 sc1_dir=$rootdir/use-cases/challenge-problem-3/cp3_scenarios/scenario1/
 maude_out=$sc1_dir/baseline.maude
 
-python3 ./scripts/cp3_glue/generate_quatex.py test.quatex
-
 # Generate small maude baselines, one per vantage point / feature pair
 python3 ./scripts/generate_cp3_v3.py $sc1_dir/pwnd_cp3_scenario_1.yaml --parallelizeBaseline --baselineTime=20.0
 
 # Run all the maude baselines
 python3 ./scripts/cp3_glue/run_parallel_maudes.py $sc1_dir/baselines 
 
+# combine all the baseline logs to produce vantage/feature jsons and a complete maude baseline actor
 python3 ./scripts/cp3_glue/parse_baseline.py $sc1_dir/baselines/logs $sc1_dir/baselines/jsons -m $maude_out
