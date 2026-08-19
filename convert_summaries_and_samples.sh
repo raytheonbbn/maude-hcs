b16e76e3a012c559c3ee050e013f8025356ca9b7
Joud Khoury <joud.khoury@rtx.com>
2026-08-18 23:00:16 -0400

    updates to convert 1811 scenario 1

--- convert_summaries_and_samples.sh
+++ convert_summaries_and_samples.sh
@@ -4,12 +4,12 @@
 
 set -euo pipefail
 
-# root_dir=/Users/jkhoury/Documents/Research/BBN/weirdnets/code/maude-hcs-fresh
-root_dir=/Users/lwest/Documents/pwnd2/playground-maude-hcs/
-scenario_dir=$root_dir/use-cases/challenge-problem-3/cp3_scenarios/scenario2
+root_dir=$(pwd)
+# root_dir=/Users/lwest/Documents/pwnd2/playground-maude-hcs/
+scenario_dir=$root_dir/use-cases/challenge-problem-3/cp3_scenarios/scenario1
 
-tne_output_dir=$scenario_dir/scenario2_7210_conf_formatted
-raw_results_dir=$scenario_dir/scenario2_7210_conf_raw
+tne_output_dir=$scenario_dir/scenario1_1811_conf_formatted
+raw_results_dir=$scenario_dir/scenario1_1811_conf_raw
 dump_dir=$raw_results_dir/dumplogs
 
 scenario1_mapping=$(cat << 'EOF'
@@ -77,14 +77,14 @@ cat $dump_dir/dump.log.* > $dump_dir/combined_dump.log
 # these arguments have them all getting dumped in the tne output directory
 python3 ./scripts/cp3_glue/format_for_tne_v2.py \
     --json-smc-results-file $raw_results_dir/smc.log \
-    --quatex-file $raw_results_dir/pwnd_cp3_scenario_2-quatex.maude \
+    --quatex-file $raw_results_dir/pwnd_cp3_scenario_1-quatex.maude \
     --perf-output-file $tne_output_dir/perf.json \
     --adv-output-file $tne_output_dir/adv.json \
     --perf-stats-output-file $tne_output_dir/perf_stats.json \
     --adv-stats-output-file $tne_output_dir/adv_stats.json \
     --dump-file $dump_dir/combined_dump.log \
     --sample-output-dir $tne_output_dir \
-    --mapping "$scenario2_mapping" \
-    --net-mapping "$scenario2_net_mapping" \
-    --scenario "scenario2_7210_conf"
+    --mapping "$scenario1_mapping" \
+    --net-mapping "$scenario1_net_mapping" \
+    --scenario "scenario1_1811_conf"
 
