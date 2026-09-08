d7308ff02837dce21351a5735523d22c8d02e688
Joud Khoury <joud.khoury@rtx.com>
2026-09-08 12:41:54 -0400

    combo 4x5 post fixes

--- convert_summaries_and_samples.sh
+++ convert_summaries_and_samples.sh
@@ -4,20 +4,12 @@
 
 set -euo pipefail
 
-<<<<<<< HEAD
 root_dir=$(pwd)
 # root_dir=/Users/lwest/Documents/pwnd2/playground-maude-hcs/
-scenario_dir=$root_dir/use-cases/challenge-problem-3/cp3_scenarios/scenario1
+scenario_dir=$root_dir/use-cases/challenge-problem-3/cp3_scenarios/scenario2
 
-tne_output_dir=$scenario_dir/scenario1_1811_conf_combo2_formatted
-raw_results_dir=$scenario_dir/scenario1_1811_conf_combo2_raw
-=======
-root_dir=/Users/jkhoury/Documents/Research/BBN/weirdnets/code/maude-hcs-fresh
-scenario_dir=$root_dir/use-cases/challenge-problem-3/cp3_scenarios/scenario1
-
-tne_output_dir=$scenario_dir/scenario1_910_conf_formatted
-raw_results_dir=$scenario_dir/scenario1_910_conf_raw
->>>>>>> 48350532 (update for 910)
+tne_output_dir=$scenario_dir/scenario2_7211_conf_combo2x5_64samples_formatted
+raw_results_dir=$scenario_dir/scenario2_7211_conf_combo2x5_64samples_raw
 dump_dir=$raw_results_dir/dumplogs
 
 scenario1_mapping=$(cat << 'EOF'
@@ -85,18 +77,14 @@ cat $dump_dir/dump.log.* > $dump_dir/combined_dump.log
 # these arguments have them all getting dumped in the tne output directory
 python3 ./scripts/cp3_glue/format_for_tne_v2.py \
     --json-smc-results-file $raw_results_dir/smc.log \
-    --quatex-file $raw_results_dir/pwnd_cp3_scenario_1-quatex.maude \
+    --quatex-file $raw_results_dir/pwnd_cp3_scenario_2-quatex.maude \
     --perf-output-file $tne_output_dir/perf.json \
     --adv-output-file $tne_output_dir/adv.json \
     --perf-stats-output-file $tne_output_dir/perf_stats.json \
     --adv-stats-output-file $tne_output_dir/adv_stats.json \
     --dump-file $dump_dir/combined_dump.log \
     --sample-output-dir $tne_output_dir \
-    --mapping "$scenario1_mapping" \
-<<<<<<< HEAD
-    --net-mapping "$scenario1_net_mapping" \
-    --scenario "scenario1_1811_conf_combo2"
-=======
-    --scenario "scenario1-910-conf"
->>>>>>> 48350532 (update for 910)
+    --mapping "$scenario2_mapping" \
+    --net-mapping "$scenario2_net_mapping" \
+    --scenario "scenario2_7211_conf_combo2x5_64samples"
 
