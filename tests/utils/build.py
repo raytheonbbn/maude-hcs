5bf39ee9dd4d511e36d758caf6fa07c4b2c10e71
luke-west-bbn <Luke.S.West@rtx.com>
2026-09-22 15:10:36 -0400

    replaced random large integer with secure uuid for temp build directories (have to prevent temp dir collision when builds aren't cached)

--- tests/utils/build.py
+++ tests/utils/build.py
@@ -1,6 +1,7 @@
 import functools
 import shutil
 import random
+import uuid
 
 from pathlib import Path
 from argparse import Namespace
@@ -53,7 +54,7 @@ def batch_convert_dir_v2(dir: Path, proto: str):
 
 def build(build_cfg: BuildConfig, run_cfg: RunConfig, src_dir: Path) -> Path:
     gen_args = build_cfg.gen_args
-    build_dir = (Path(run_cfg.temp_dir) / f"{build_cfg.name}-{random.randint(0, 10000000)}").resolve()
+    build_dir = (Path(run_cfg.temp_dir) / f"{build_cfg.name}-{uuid.uuid4().hex}").resolve()
     shutil.copytree(src_dir, build_dir)
 
     for dir, proto in build_cfg.markov_v1_dirs:
