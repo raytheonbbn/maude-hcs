2d207047bb26e7e9b4c537aa78d00b68b4f0213f
luke-west-bbn <Luke.S.West@rtx.com>
2026-09-23 10:57:33 -0400

    added requirements.txt to hopefully nudge pip in the right direction re: dependency resolution, updated reset-env.sh accordingly

--- reset_env.sh
+++ reset_env.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+# YOU MUST RUN THIS FROM THE REPO ROOT
+
 set -uxo pipefail
 
-source /opt/anaconda3/etc/profile.d/conda.sh
+# Initialize conda for this script, otherwise conda commands are likely to fail
+# For interactive terminals this is done through .bashrc or .bashprofile, but that doesn't work in scripts.
+eval "$(conda shell.bash hook)"
 
-conda deactivate
+# Keep deactivating until we're back out of any conda environment.
+while [ ! -z "$CONDA_PREFIX" ]; do conda deactivate; done
+
 conda env remove --name maude-hcs
-conda create --name maude-hcs python=3.12.4
+conda create --name maude-hcs python=3.14.7
 conda activate maude-hcs
-conda install pip
+conda update pip
 
 pushd maude_hcs/deps/dns_formalization
 pip install -e .
 
 popd
-pip install -e ".[test]"
+pip install -r requirements.txt
+
